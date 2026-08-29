afcfd225b0f09a5a228404c4255bd8daa4ab6abe
Yorly Quintero <yqpaez@gmail.com>
2026-08-28 21:39:43 -0500

    chore(nvim): apply transparency and borders to floating windows

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -1,3 +1,27 @@
 return {
-    "rebelot/kanagawa.nvim"
+  "rebelot/kanagawa.nvim", 
+  lazy = false,
+  priority = 1000,
+  opts = {
+    -- Remove the background of LineNr, {Sign,Fold}Column and friends
+    colors = {
+      theme = {
+        all = {
+          ui = {
+            bg_gutter = "none"
+          }
+        }
+      }
+    },
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        NormalFloat = { bg = "none" },
+        FloatBorder = { bg = "none" },
+        FloatTitle  = { bg = "none" },
+        LazyNormal  = { bg = "none", fg = theme.ui.fg_dim },
+        MasonNormal = { bg = "none", fg = theme.ui.fg_dim },
+      }
+    end,
+  } 
 }
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,10 @@
 return {
-    "mason-org/mason.nvim",
-    opts = {}
+  "mason-org/mason.nvim",
+  opts = {
+    ui = {
+      -- The backdrop opacity. 0 is fully opaque, 100 is fully transparent.
+      backdrop = 100,
+      border = "rounded",
+    }
+  }
 }
